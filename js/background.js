ba38010eb375a742a18cdf6656f22f1b91351116
Paul Conroy <paul@daft.ie>
2014-03-13 09:04:33 +0000

    Check last update to see if it's an emoji

--- js/background.js
+++ js/background.js
@@ -1,5 +1,26 @@
 // Only fire if we're onthe PR page
-if(document.URL.match(/\/pulls/g))
+if(document.URL.match(/\/pulls(\/?)/g))
+{
+    processEmoji();
+}
+
+// Clicking the "My Pull Requests" links
+$('.filter-item').click(function(){
+    // Slight delay to allow ajax request to complete
+    // @TODO: Piggy-back on ajax request to make this a bit less flaky, or use
+    // mutation observers, as ajax call doesn't seem to fire when you
+    // click back and forth repeatedly between user links
+    setTimeout(processEmoji, 1000);
+});
+
+
+/**
+ * Check all PRs on the page and see if emojis have been posted on the PR
+ * pages linked.
+ *
+ * @return null
+ */
+function processEmoji()
 {
     var links = $('h4:has(> a) > a');
     links.each(function(i){
@@ -34,11 +55,32 @@ if(document.URL.match(/\/pulls/g))
                             '" height="16" width="16" valign="top" />&nbsp;';
                     });
 
+                    // Possible that there was a further update after the last emoji
+                    // Check if the last significant entry didn't have an emoji
+                    var comments = $(data).find('.js-discussion > div');
+
+                    // Last item is socket-related, ignore it
+                    var lastComment = comments[comments.length-2];
+                    var lastEmoji = $(lastComment).find('img.emoji');
+                    // No emoji found? Something changed since the last one.
+                    if (lastEmoji.length == 0)
+                    {
+                        var avatar = $(lastComment).find('img.js-avatar').attr('src');
+                        emojiString += '<img src="' + avatar +
+                            '" height="16" width="16" valign="top" /> ';
+                        // Add in a "speech bubble" emoticon to indicate further
+                        // conversation (may have been commit or comment - key
+                        // thing being it's a change)
+                        emojiString += '<img src="' +
+                            'https://github.global.ssl.fastly.net/images/icons/emoji/speech_balloon.png' +
+                            '" height="16" width="16" valign="top" />&nbsp;';
+                    }
                     // Get link from this page to figure out the parent
                     // we need to update
                     var parentLink = $(data).find('.tabnav-tab.selected.js-pull-request-tab').attr('href');
                     // Update the link on the /pulls/ page with a "Pr status" block
-                    $('h4:has(> a) > a[href$="' + parentLink + '"]').parent().append('<br /><div class="approval-status"><i>Review status: ' + emojiString + '</i></div>');
+                    $('h4:has(> a) > a[href$="' + parentLink + '"]').parent()
+                        .append('<br /><div class="approval-status"><i>Review status: ' + emojiString + '</i></div>');
                 }
             });
     });
